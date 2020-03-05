85fd44276120d3c04728a7d702f79574dfc8573e
Timjoo <timjoo@sk.com>
2020-03-05 14:10:41 +0900

    modifying

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><h1>TaskAgile</h1></div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,21 +1,9 @@
-// For authoring Nightwatch tests, see
-// https://nightwatchjs.org/guide
-
 module.exports = {
-  'default e2e tests': browser => {
+  'login test': function (browser) {
     browser
-      .init()
-      .waitForElementVisible('#app')
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  },
-
-  'example e2e test using a custom command': browser => {
-    browser
-      .openHomepage()
-      .assert.elementPresent('.hello')
+      .url(process.env.VUE_DEV_SERVER_URL + 'login')
+      .waitForElementVisible('#app',5000)
+      .assert.containsText('h1','TaskAgile')
       .end()
   }
 }
